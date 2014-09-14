1dcb006c1ac839f5ba817bc7dc65e78a9bb2d991
achillean <achillean@shodanhq.com>
2014-09-14 14:32:54 -0700

    .

---
+++ shodan/threatnet.py
@@ -0,0 +1,49 @@
+import requests
+import simplejson
+
+class Threatnet:
+    """Wrapper around the Threatnet REST and Streaming APIs
+
+    :param key: The Shodan API key that can be obtained from your account page (https://account.shodan.io)
+    :type key: str
+    :ivar stream: An instance of `shodan.Threatnet.Stream` that provides access to the Streaming API.
+    """
+    
+    class Stream:
+
+        base_url = 'https://stream.shodan.io'
+
+        def __init__(self, parent):
+            self.parent = parent
+
+        def _create_stream(self, name):
+            try:
+                req = requests.get(self.base_url + name, params={'key': self.parent.api_key}, stream=True)
+            except:
+                raise APIError('Unable to contact the Shodan Streaming API')
+
+            if req.status_code != 200:
+                try:
+                    raise APIError(data.json()['error'])
+                except:
+                    pass
+                raise APIError('Invalid API key or you do not have access to the Streaming API')
+            return req
+
+        def activity(self):
+            stream = self._create_stream('/threatnet/activity')
+            for line in stream.iter_lines():
+                if line:
+                    banner = simplejson.loads(line)
+                    yield banner
+    
+    def __init__(self, key):
+        """Initializes the API object.
+        
+        :param key: The Shodan API key.
+        :type key: str
+        """
+        self.api_key = key
+        self.base_url = 'https://api.shodan.io'
+        self.stream = self.Stream(self)
+

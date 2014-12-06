52076c1fe6d61c38a3d59bc15bc896684a56ae3f
achillean <jmath@shodan.io>
2014-12-06 01:50:16 -0600

    Incremented version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name = 'shodan',
-    version = '1.2.0',
+    version = '1.2.1',
     description = 'Python library and command-line utility for Shodan (https://developer.shodan.io)',
     author = 'John Matherly',
     author_email = 'jmath@shodan.io',

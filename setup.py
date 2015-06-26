084758e956a47e026800d939c4eeb45dd2909b01
0x90 <oleg.kupreev@gmail.com>
2015-06-26 23:31:15 +0300

    + .gitignore ! patched setup.py to use setuptools

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup
 
 setup(
-    name = 'shodan',
-    version = '1.3.3',
-    description = 'Python library and command-line utility for Shodan (https://developer.shodan.io)',
-    author = 'John Matherly',
-    author_email = 'jmath@shodan.io',
-    url = 'http://github.com/achillean/shodan-python/tree/master',
-    packages = ['shodan'],
-    scripts = ['bin/shodan'],
+    name='shodan',
+    version='1.3.3',
+    description='Python library and command-line utility for Shodan (https://developer.shodan.io)',
+    author='John Matherly',
+    author_email='jmath@shodan.io',
+    url='http://github.com/achillean/shodan-python/tree/master',
+    packages=['shodan'],
+    scripts=['bin/shodan'],
     install_requires=["simplejson", "requests", "click", "colorama"],
-    classifiers = [
+    classifiers=[
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',

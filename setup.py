f05207abcab831b52a72cd754136f5480167fe8b
achillean <jmath@shodan.io>
2015-07-03 18:25:05 -0500

    Bumped version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name = 'shodan',
-    version = '1.3.4',
+    version = '1.3.5',
     description = 'Python library and command-line utility for Shodan (https://developer.shodan.io)',
     author = 'John Matherly',
     author_email = 'jmath@shodan.io',

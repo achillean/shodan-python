1d8aad75c4be33c54f84b98fd60518bdca1e785e
John Matherly <jmath@shodan.io>
2015-12-04 17:12:11 -0600

    Define a minimum version for the requests library (#25) Bumped version

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ from setuptools import setup
 
 setup(
     name = 'shodan',
-    version = '1.4.0',
+    version = '1.4.1',
     description = 'Python library and command-line utility for Shodan (https://developer.shodan.io)',
     author = 'John Matherly',
     author_email = 'jmath@shodan.io',
     url = 'http://github.com/achillean/shodan-python/tree/master',
     packages = ['shodan'],
     scripts = ['bin/shodan'],
-    install_requires=["simplejson", "requests", "click", "colorama"],
+    install_requires=["simplejson", "requests>=2.2.1", "click", "colorama"],
     classifiers = [
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',

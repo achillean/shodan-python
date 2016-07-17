019a1b0692246f5806803c1cbb8b788290ac7403
John Matherly <jmath@shodan.io>
2016-07-17 12:55:46 -0500

    Bumped version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name = 'shodan',
-    version = '1.5.4',
+    version = '1.5.5',
     description = 'Python library and command-line utility for Shodan (https://developer.shodan.io)',
     author = 'John Matherly',
     author_email = 'jmath@shodan.io',

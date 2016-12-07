479ed0a4df06cfa76c52fdd7390eeb127ec415b9
achillean <jmath@shodan.io>
2016-12-07 02:06:26 -0600

    Allow the development of plugins for the CLI

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ from setuptools import setup
 
 setup(
     name = 'shodan',
-    version = '1.5.6',
+    version = '1.6.0',
     description = 'Python library and command-line utility for Shodan (https://developer.shodan.io)',
     author = 'John Matherly',
     author_email = 'jmath@shodan.io',
     url = 'http://github.com/achillean/shodan-python/tree/master',
     packages = ['shodan', 'shodan.cli', 'shodan.cli.converter'],
     scripts = ['bin/shodan'],
-    install_requires=["simplejson", "requests>=2.2.1", "click", "colorama"],
+    install_requires=["simplejson", "requests>=2.2.1", "click", "click-plugins", "colorama"],
     classifiers = [
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',

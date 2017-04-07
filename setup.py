d897f9a64d8949ad63360d2094c443d28ef05f11
John Matherly <jmath@shodan.io>
2017-04-07 17:16:29 -0500

    Fixed bug where the "stream" command didn't print nested properties

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name = 'shodan',
-    version = '1.6.4',
+    version = '1.6.5',
     description = 'Python library and command-line utility for Shodan (https://developer.shodan.io)',
     author = 'John Matherly',
     author_email = 'jmath@shodan.io',

8dd9b293fc4db1e0cacc987eb1eabb96974de473
John Matherly <jmath@shodan.io>
2017-06-09 14:29:13 -0500

    New release to apply fix from #40

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name = 'shodan',
-    version = '1.6.8',
+    version = '1.6.9',
     description = 'Python library and command-line utility for Shodan (https://developer.shodan.io)',
     author = 'John Matherly',
     author_email = 'jmath@shodan.io',

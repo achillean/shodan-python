7e24f4b447f764391bdc00f30de3a8abbfe75d99
cclauss <cclauss@bluewin.ch>
2018-10-07 09:12:57 +0200

    Remove pylint: disable=E0602
    This PR fixes PyLint's complaint.

--- shodan/cli/converter/csvc.py
+++ shodan/cli/converter/csvc.py
@@ -81,7 +81,6 @@ class CsvConverter(Converter):
         for k, v in d.items():
             new_key = parent_key + sep + k if parent_key else k
             if isinstance(v, MutableMapping):
-                # pylint: disable=E0602
                 items.extend(self.flatten(v, new_key, sep=sep).items())
             else:
                 items.append((new_key, v))
